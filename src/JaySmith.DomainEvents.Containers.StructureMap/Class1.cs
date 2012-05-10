1aed2f88acc84a24d199360c26bc76c2e4463ec6
Jay Smith <jay@jaysmith.us>
2012-05-10 10:14:58 -0500

    clean up

---
+++ src/JaySmith.DomainEvents.Containers.StructureMap/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaySmith.DomainEvents.Containers.StructureMap
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/JaySmith.DomainEvents/DomainEventManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaySmith.DomainEvents
+{
+    using System.Web.Mvc;
+
+    public class DomainEventManager
+    {
+        [ThreadStatic] //so that each thread has its own callbacks   
+        private static List<Delegate> actions;
+
+        public static IDependencyResolver Container { get; set; }
+
+        //Registers a callback for the given domain event   
+        public static void Register<T>(Action<T> callback) where T : IDomainEvent
+        {
+            if (actions == null)
+                actions = new List<Delegate>();
+
+            actions.Add(callback);
+        }
+
+        // Clears callbacks passed to Register on the current thread  
+        public static void ClearCallbacks()
+        {
+            actions = null;
+        }
+
+        // Raises the given domain event  
+        public static void Raise<T>(T args) where T : IDomainEvent
+        {
+            DependencyResolver.Current.GetServices<IDomainEventHandler<T>>();
+
+            foreach (var handler in Container.GetServices<IDomainEventHandler<T>>())
+                handler.Handle(args);
+
+            if (actions != null)
+                foreach (var action in actions)
+                    if (action is Action<T>)
+                        ((Action<T>)action)(args);
+        }
+
+    }
+}
