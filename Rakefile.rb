59dfd078f48c486bc2bab3e8483faf5e562c15ca
= <=>
2012-08-28 14:50:59 -0500

    Added new nuget packages

---
+++ Rakefile.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+require 'albacore'
+require 'fileutils'
+
+CONFIG        = 'Debug'
+RAKE_DIR      = File.expand_path(File.dirname(__FILE__))
+SOLUTION_DIR  = RAKE_DIR + "/src"
+SRC_DIR       = SOLUTION_DIR + "/src/"
+SOLUTION_FILE = 'JaySmith.DomainEvents.sln'
+NUGET         = SOLUTION_DIR + "/.nuget/nuget.exe"
+
+# --------------------------------------------------------------------------------------------
+task :default                     => ['build:msbuild']
+task :package                     => ['package:packall']
+task :push                        => ['package:pushall']
+
+namespace :build do
+
+  msbuild :msbuild, [:targets] do |msb, args|
+    args.with_defaults(:targets => :Build)
+    msb.properties :configuration => CONFIG
+    msb.targets args[:targets]
+    msb.solution = "#{SOLUTION_DIR}/#{SOLUTION_FILE}"
+  end
+end
+
+namespace :package do
+	
+	def create_packs()
+		sh 'src/.nuget/nuget.exe pack src/JaySmith.DomainEvents/JaySmith.DomainEvents.csproj -o pack'
+		sh 'src/.nuget/nuget.exe pack src/JaySmith.DomainEvents.StructureMap/JaySmith.DomainEvents.StructureMap.csproj -o pack'
+	end
+		
+	task :packall => [ :clean ] do
+		Dir.mkdir('pack')
+		create_packs	
+		Dir.glob('pack/*') { |file| FileUtils.move(file,'nuget/') }
+		Dir.rmdir('pack')
+	end
+	
+	task :pushall => [ :clean ] do
+		Dir.mkdir('pack')
+		create_packs	
+		Dir.chdir('pack')
+		Dir.glob('*').each do |file| 
+			sh '../Highway/.nuget/nuget.exe push ' + file
+			FileUtils.move(file,'../nuget/')
+		end
+		Dir.chdir('..')
+		Dir.rmdir('pack')
+	end
+	
+	task :clean do
+		if Dir.exists? 'pack' 
+			Dir.chdir('pack')
+			Dir.glob('*').each do |file| 
+				rm file
+			end
+			Dir.chdir('..')
+			Dir.rmdir('pack')
+		end
+	end
+end
--- src/JaySmith.DomainEvents/Properties/AssemblyInfo.cs
+++ src/JaySmith.DomainEvents/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JaySmith.DomainEventManager")]
+[assembly: AssemblyTitle("JaySmith.DomainEvents")]
 [assembly: AssemblyDescription("A Simple Domain Event framework for applications based on work by Udi Dahan")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jay Smith")]
-[assembly: AssemblyProduct("JaySmith.DomainEventManager")]
+[assembly: AssemblyProduct("JaySmith.DomainEvents")]
 [assembly: AssemblyCopyright("Copyright © Jay Smith 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyFileVersion("2.0.0.3")]
